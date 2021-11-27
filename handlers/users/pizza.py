1e25f501415ede69274da1f176ff0e458aa8f6e7
aeroalgo <j,jccsct,tt,fkj11>
2021-11-27 15:40:23 +0300

    pizza bot version 2

--- handlers/users/pizza.py
+++ handlers/users/pizza.py
@@ -20,38 +20,51 @@ async def taking_order_handler(message: types.Message):
 async def cmd_reset(message: types.Message):
     for user in users_queue:
         if message.from_user.id == user:
-            users_queue[message.from_user.id] = PizzaFsm()
+            users_queue[user].machine.set_state('Ждем заказ')
             await message.answer("Какую вы хотите пиццу? Большую или маленькую?")
+
+
+@dp.message_handler(commands=['cancel'])
+async def cmd_reset(message: types.Message):
+    for user in users_queue:
+        if message.from_user.id == user:
+            users_queue[user].machine.set_state('Ждем заказ')
+            await message.answer("Заказ отменен! Чтобы заказать снова, нажмите /pizza")
 
 
 @dp.message_handler()
 async def order_handler(message: types.Message):
     for user in users_queue:
-        if message.from_user.id == user:
-            if message.text.capitalize() not in users_queue[user].command:
-                await message.answer('Нет такой команды, попробуйте снова')
+        try:
+            if message.from_user.id == user:
+                if message.text.capitalize() not in users_queue[user].command:
+                    await message.answer('Нет такой команды, попробуйте снова')
 
-            if message.text.capitalize() == 'Большую':
-                users_queue[user].big()
-                await message.answer('Как вы будете платить, наличкой или банковской картой?')
-            elif message.text.capitalize() == 'Маленькую':
-                users_queue[user].small()
-                await message.answer('Как вы будете платить, наличкой или банковской картой?')
+                if message.text.capitalize() == 'Большую':
+                    users_queue[user].big()
+                    users_queue[user].size = users_queue[user].state
+                    await message.answer('Как вы будете платить, наличкой или банковской картой?')
+                elif message.text.capitalize() == 'Маленькую':
+                    users_queue[user].small()
+                    users_queue[user].size = users_queue[user].state
+                    await message.answer('Как вы будете платить, наличкой или банковской картой?')
 
-            if message.text.capitalize() == 'Наличкой':
-                users_queue[user].cash()
-                await message.answer(f'Вы хотите {users_queue[user].size} пиццу, оплата - {users_queue[user].payment}?')
+                if message.text.capitalize() == 'Наличкой':
+                    users_queue[user].cash()
+                    users_queue[user].payment = users_queue[user].state
+                    await message.answer(
+                        f'Вы хотите {users_queue[user].size} пиццу, оплата - {users_queue[user].payment}?')
+                elif message.text.capitalize() in ['Картой', 'Банковской картой']:
+                    users_queue[user].with_card()
+                    users_queue[user].payment = users_queue[user].state
+                    await message.answer(
+                        f'Вы хотите {users_queue[user].size} пиццу, оплата - {users_queue[user].payment}?')
+                if message.text.capitalize() == 'Да':
+                    users_queue[user].yes()
+                    await message.answer('Спасибо за заказ! Чтобы заказать снова, нажмите /pizza')
 
-            elif message.text.capitalize() in ['Картой', 'Банковской картой']:
-                users_queue[user].with_card()
-                await message.answer(f'Вы хотите {users_queue[user].size} пиццу, оплата - {users_queue[user].payment}?')
-
-            if message.text.capitalize() == 'Да':
-                users_queue[user].yes()
-                await message.answer(f'Спасибо за заказ! Чтобы заказать снова, нажмите /pizza')
-                del users_queue[user]
-
-            elif message.text.capitalize() == 'Нет':
-                users_queue[user].no()
-                await message.answer(f'Заказ отменен! Чтобы заказать снова, нажмите /pizza')
-                del users_queue[user]
+                elif message.text.capitalize() == 'Нет':
+                    users_queue[user].no()
+                    await message.answer('Заказ отменен! Чтобы заказать снова, нажмите /pizza')
+        except:
+            await message.answer('Продолжите или отмените заказ /cancel')
--- states/pizza.py
+++ states/pizza.py
@@ -1,7 +1,7 @@
 from transitions import Machine
 
 
-class PizzaFsm:
+class PizzaFsm(object):
     size = None
     payment = None
     command = [
@@ -19,25 +19,23 @@ class PizzaFsm:
         'Маленькую',
         'Наличкой',
         'Банковской картой',
+        'Конец заказа',
     ]
 
     transitions = [
-        {'trigger': 'big', 'source': 'Ждем заказ', 'dest': 'Большую', 'after': 'size'},
-        {'trigger': 'small', 'source': 'Ждем заказ', 'dest': 'Маленькую', 'after': 'size'},
-        {'trigger': 'cash', 'source': ['Большую', 'Маленькую'], 'dest': 'Наличкой', 'after': 'payment'},
-        {'trigger': 'with_card', 'source': ['Большую', 'Маленькую'], 'dest': 'Банковской картой', 'after': 'payment'},
-        {'trigger': 'yes', 'source': ['Банковской картой', 'Наличкой'], 'dest': 'Ждем заказ'},
-        {'trigger': 'no', 'source': ['Банковской картой', 'Наличкой'], 'dest': 'Ждем заказ'},
-        # {'trigger': 'remove', 'source': ['Банковской картой', 'Наличкой', 'Большую', 'Маленькую'], 'dest': 'Ждем заказ',
-        #  'after': 'clear'}
+        {'trigger': 'big', 'source': 'Ждем заказ', 'dest': 'Большую'},
+        {'trigger': 'small', 'source': 'Ждем заказ', 'dest': 'Маленькую'},
+        {'trigger': 'cash', 'source': ['Большую', 'Маленькую'], 'dest': 'Наличкой'},
+        {'trigger': 'with_card', 'source': ['Большую', 'Маленькую'], 'dest': 'Банковской картой'},
+        {'trigger': 'yes', 'source': ['Банковской картой', 'Наличкой'], 'dest': 'Конец заказа',
+         'after': 'set_state_end_order'},
+        {'trigger': 'no', 'source': ['Банковской картой', 'Наличкой'], 'dest': 'Конец заказа',
+         'after': 'set_state_end_order'},
     ]
 
     def __init__(self):
         self.machine = Machine(model=self, states=PizzaFsm.states, transitions=PizzaFsm.transitions,
                                initial='Ждем заказ')
 
-    def size(self):
-        self.size = self.state
-
-    def payment(self):
-        self.payment = self.state
+    def set_state_end_order(self):
+        self.machine.set_state('Ждем заказ')
