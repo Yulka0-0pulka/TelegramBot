22675c884e10c7470e0bdb1e5a7df855d305060a
Yulia T <iul1a.1u1@yandex.ru>
2023-04-23 15:30:57 +0300

    Revert "init"
    This reverts commit 9d6236fe2c89639ca0c0fa4c0e2f0256c73e567e.

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from aiogram import executor
 
 from loader import dp
+import middlewares, filters, handlers
 from utils.notify_admins import on_startup_notify
 from utils.set_bot_commands import set_default_commands
 from handlers.users.help import bot_help
 from handlers.users.pizza import taking_order_handler
+
 
 async def on_startup(dispatcher):
     # Устанавливаем дефолтные команды
@@ -16,3 +18,4 @@ async def on_startup(dispatcher):
 
 if __name__ == '__main__':
     executor.start_polling(dp, on_startup=on_startup)
+
