fbc2fbf8ce26f0a390caf8e1d191e853756995c8
Julia <***@example.com>
2023-12-19 21:40:05 +0300

    x

--- handlers/users/content.py
+++ handlers/users/content.py
@@ -1,11 +1,14 @@
 import asyncio
+from concurrent.futures import ProcessPoolExecutor
+from csv import excel
+from functools import partial
 from itertools import count
 import re
 
 from setuptools import Command
 from data.maping import channels
 from model.models import Replay, Topic
-from parser.asa import get_download_content
+from parser.parser import get_download_content
 from aiogram import types
 from aiogram.dispatcher import FSMContext
 from loader import dp
@@ -17,45 +20,53 @@ from sqlalchemy import delete
 
 @dp.message_handler(commands=['photo'])
 async def taking_order_handler(message: types.Message):
+    loop = asyncio.get_running_loop()
     is_sent: dict[int, bool] = {x: False for x in channels}
     while True:
         for chanel in channels:
-            if not is_sent.get(chanel):
-                object = session.query(Topic).filter(
-                    Topic.chanel_id == str(chanel)
-                ).first()
-                try:
-                    insert_image(
-                        url=object.url, content_type=object.content_type,
-                        model=Replay, chanel_id=object.chanel_id
-                    )
-                except:
-                    pass
-                session.delete(object)
-                session.commit()
-                try:
-                    if object.content_type in ['image/jpeg', 'image/png']:
-                        await bot.send_photo(object.chanel_id, object.url)
-                    elif object.content_type == "video/mp4":
-                        await bot.send_video(object.chanel_id, object.url)
-                    is_sent[chanel] = True
-                except Exception as e:
-                    print("-----------------------------------")
-                    print(e)
-                    print(chanel)
-                    print("-----------------------------------")
-                    continue
+            image_obj = session.query(Topic).filter(
+                Topic.chanel_id == str(chanel)
+            ).first()
+            if image_obj:
+                insert_image(
+                    url=image_obj.url, content_type=image_obj.content_type,
+                    model=Replay, chanel_id=image_obj.chanel_id
+                )
+            else:
+                continue
+            session.delete(image_obj)
+            session.commit()
+            try:
+                if image_obj.content_type in ['image/jpeg', 'image/png']:
+                    await bot.send_photo(image_obj.chanel_id, image_obj.url)
+                elif image_obj.content_type == "video/mp4":
+                    await bot.send_video(image_obj.chanel_id, image_obj.url)
+            except Exception as e:
+                print("-----------------------------------")
+                print(e)
+                print(chanel)
+                print("-----------------------------------")
+                continue
+        else:
+            tasks = []
+            with ProcessPoolExecutor() as ex:
+                for chanel in channels:
+                    count = session.query(Topic).filter(
+                        Topic.chanel_id == str(chanel)
+                    ).count()
+                    if count < 20:
+                        tasks.append(asyncio.create_task(
+                            download_content(chanel, loop, ex))
+                        )
+                await asyncio.gather(*tasks)
+
         d = datetime.now() - timedelta(days=30)
         stmt = delete(Replay).where(Replay.date_update <= d)
         session.execute(stmt)
         session.commit()
-        if all(is_sent.values()):
-            for chanel in channels:
-                count = session.query(Topic).filter(
-                    Topic.chanel_id == str(chanel)
-                ).count()
-                if count < 20:
-                    get_download_content(chanel)
-                is_sent[chanel] = False
 
-            await asyncio.sleep(100)
+        await asyncio.sleep(100)
+
+
+async def download_content(channel, loop, ex):
+    await loop.run_in_executor(ex, partial(get_download_content, channel))
--- utils/notify_admins.py
+++ utils/notify_admins.py
@@ -7,11 +7,7 @@ from data.config import ADMINS
 
 async def on_startup_notify(dp: Dispatcher):
     for admin in ADMINS:
-
         try:
             await dp.bot.send_message(admin, "Добро пожаловать!")
-
         except Exception as err:
             logging.exception(err)
-    
-            
